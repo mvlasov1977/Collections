fb2ddb73a02c5e292eae59372b88c10bd1c1327d
Maksim Vlasov <mvlasov1977@gmail.com>
2023-06-14 02:19:49 +0300

    second exercise done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -50,7 +50,77 @@ public class Main {
         myLinkedHashSet.measResult.printRunTimeStatistics();
         myLinkedHashSet.printCollection(myLinkedHashSet.myCollectionWithoutDuplicates);
 
+       /*
+        2.У колі стоять N осіб, пронумерованих від 1 до N. Під час ведення рахунку
+        по колу викреслюється кожна друга людина, доки залишиться одна.
+        Скласти дві програми, що моделюють процес. Одна із програм
+        повинна використовувати клас ArrayList, а друга – LinkedList. Яка з
+        двох програм працює швидше? Чому?
+       */
 
+        int maxCount = 9;
+        ArrayList <Integer> personArrayList = new ArrayList<>();
+        LinkedList <Integer> personLinkedList = new LinkedList();
+        int countFirst = 1;
+        int countSecond = 1;
+        for (int i = 0; i < maxCount; i++){
+            personArrayList.add(countFirst);
+            personLinkedList.add(countSecond);
+            countFirst++;
+            countSecond++;
+        }
+
+        RunTimeStatistics arrayListResult = new RunTimeStatistics("ArrayList");
+        System.out.println(personArrayList);
+        int itemCount = 0;
+        int iteration = 0;
+
+        long startTimePoint = System.nanoTime();
+        while (personArrayList.size() > 1){
+            iteration++;
+            System.out.print("(" + iteration + ") removed: ");
+            for (Iterator myElement = personArrayList.iterator(); myElement.hasNext();){
+                Object myItem = myElement.next();
+                itemCount++;
+                if (itemCount == 2){
+                    System.out.print(myItem + " ");
+                    myElement.remove();
+                    itemCount = 0;
+                }
+            }
+            System.out.println("");
+        }
+        long stopTimePoint = System.nanoTime();
+        arrayListResult.addRunTimePoint(startTimePoint, stopTimePoint, "every second leave");
+        System.out.println(personArrayList);
+        System.out.println("");
+
+        RunTimeStatistics LinkedListResult = new RunTimeStatistics("LinkedList");
+        System.out.println(personLinkedList);
+         itemCount = 0;
+         iteration = 0;
+        startTimePoint = System.nanoTime();
+        while (personLinkedList.size() > 1){
+            iteration++;
+            System.out.print("(" + iteration + ") removed: ");
+            for (Iterator myElement = personLinkedList.iterator(); myElement.hasNext();){
+                Object myItem = myElement.next();
+                itemCount++;
+                if (itemCount == 2){
+                    System.out.print(myItem + " ");
+                    myElement.remove();
+                    itemCount = 0;
+                }
+            }
+            System.out.println("");
+        }
+        stopTimePoint = System.nanoTime();
+        LinkedListResult.addRunTimePoint(startTimePoint, stopTimePoint, "every second leave");
+        System.out.println(personLinkedList);
+        System.out.println("");
+
+        arrayListResult.printRunTimeStatistics();
+        LinkedListResult.printRunTimeStatistics();
 
     }
 }
