7fb0773304e402c51a9d39fcb65d785a26f0b5b8
Maksim Vlasov <mvlasov1977@gmail.com>
2023-06-15 00:19:43 +0300

    third exercise done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,10 +16,12 @@ public class Main {
         int sizeOfList = 100;
         int maximumValue = 50;
 
+        // будую колекцію випадкових чисел
         CollectionDiscovery myTestNumbers = new CollectionDiscovery("Test numbers", CollectionDiscovery.listType.ArrayList,sizeOfList, maximumValue);
         myTestNumbers.measResult.printRunTimeStatistics();
         myTestNumbers.printCollection(myTestNumbers.myCollection);
 
+        // видаляю дублікати з підрахунком видалених , для різних типів колекцій (дані однакові)
 
         CollectionDiscovery myArrayList = new CollectionDiscovery(myTestNumbers.myCollection,"ArrayList",CollectionDiscovery.listType.ArrayList);
         myArrayList.measResult.printRunTimeStatistics();
@@ -58,7 +60,8 @@ public class Main {
         двох програм працює швидше? Чому?
        */
 
-        int maxCount = 9;
+        // розставляємо людей
+        int maxCount = 50;
         ArrayList <Integer> personArrayList = new ArrayList<>();
         LinkedList <Integer> personLinkedList = new LinkedList();
         int countFirst = 1;
@@ -70,6 +73,7 @@ public class Main {
             countSecond++;
         }
 
+        // викидаємо кожного другого з ArrayList
         RunTimeStatistics arrayListResult = new RunTimeStatistics("ArrayList");
         System.out.println(personArrayList);
         int itemCount = 0;
@@ -95,6 +99,7 @@ public class Main {
         System.out.println(personArrayList);
         System.out.println("");
 
+        // викидаємо кожного другого з LinkedList
         RunTimeStatistics LinkedListResult = new RunTimeStatistics("LinkedList");
         System.out.println(personLinkedList);
          itemCount = 0;
@@ -119,8 +124,44 @@ public class Main {
         System.out.println(personLinkedList);
         System.out.println("");
 
+        //виводжу результати замірів
         arrayListResult.printRunTimeStatistics();
+        System.out.println("");
         LinkedListResult.printRunTimeStatistics();
+        System.out.println("");
 
+       /*
+        3.Задано список цілих чисел та число X. Не використовуючи допоміжних
+          об'єктів і не змінюючи розмір списку, переставити елементи списку так,
+          щоб спочатку йшли числа, що не перевищують X, а потім числа, більші за X.
+       */
+
+        int X = 10;
+
+        // create ArrayList data
+        List<Integer> newArrayList = new ArrayList<Integer>();
+        newArrayList.add(5);
+        newArrayList.add(10);
+        newArrayList.add(17);
+        newArrayList.add(7);
+        newArrayList.add(4);
+        newArrayList.add(20);
+        newArrayList.add(1);
+        newArrayList.add(1);
+
+        //print ArrayList
+        System.out.println("Input  array list: " + newArrayList);
+
+        // алгоритм "бульбашки", зліва завжди буде менше , справа більше.  Без доміжних об'єктів!
+        for (int myIteration=0, arraySize = newArrayList.size(); myIteration < arraySize-1; myIteration++) {
+            for (int indexArray = 0, myCount = newArrayList.size() ; indexArray < myCount-1; indexArray++) {
+                if ( newArrayList.get(indexArray) > newArrayList.get(indexArray + 1)){ // міняю місцями якщо В більше за А
+                    newArrayList.set(indexArray, newArrayList.get(indexArray) + newArrayList.get(indexArray + 1)); // A = A + B
+                    newArrayList.set(indexArray + 1, newArrayList.get(indexArray) - newArrayList.get(indexArray + 1)); // B = A - B
+                    newArrayList.set(indexArray, newArrayList.get(indexArray) - newArrayList.get(indexArray + 1)); // A = A - B
+                }
+            }
+        }
+        System.out.println("Result array list: " + newArrayList);
     }
 }
